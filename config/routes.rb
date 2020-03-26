8c636b1e9a064b8a7c00b01c861454d764d348cd
reinatakemura <sleepfreemusic.tb@gmail.com>
2020-03-26 21:47:05 +0900

    initial commit
    - トップページの作成
    - サインアップ・サインイン機能
    - リスト機能
    - カード機能
    実装ずみ

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  devise_for :users, controllers: {registrations: 'users/registrations'}
+  root 'top#index'
+  resources :list, only: [:new, :create, :edit, :update, :destroy] do
+    resources :card, except: :index
+  end
+end
